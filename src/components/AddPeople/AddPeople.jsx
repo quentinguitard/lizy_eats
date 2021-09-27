78d27e3239d850acfad5e8e5c9e84335309d5e9f
Quentin Guitard <quentinguitard@MBPdeQuentin2.fritz.box>
2021-09-27 11:24:52 +0200

    netlify config

--- src/components/AddPeople/AddPeople.jsx
+++ src/components/AddPeople/AddPeople.jsx
@@ -18,12 +18,15 @@ export default function AddPeople({ setAddPeople, setPeople, people }) {
 
   useEffect(() => {
     const getCategories = async () => {
-      const response = await axios.get("https://api.yelp.com/v3/categories", {
-        headers: {
-          Authorization: `Bearer ${process.env.REACT_APP_YELP_API_KEY}`,
-          withCredentials: true,
-        },
-      });
+      const response = await axios.get(
+        "/cors-proxy/https://api.yelp.com/v3/categories",
+        {
+          headers: {
+            Authorization: `Bearer ${process.env.REACT_APP_YELP_API_KEY}`,
+            withCredentials: true,
+          },
+        }
+      );
 
       const sortRestaurantCategories = response.data.categories.filter(
         (value) => value.parent_aliases.includes("restaurants")

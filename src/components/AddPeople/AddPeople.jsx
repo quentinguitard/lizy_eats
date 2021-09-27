795d7db5970f87d41dcbe64be9e5bf5d6b9951a9
Quentin Guitard <quentinguitard@MBPdeQuentin2.fritz.box>
2021-09-27 12:03:33 +0200

    test cors

--- src/components/AddPeople/AddPeople.jsx
+++ src/components/AddPeople/AddPeople.jsx
@@ -18,7 +18,7 @@ export default function AddPeople({ setAddPeople, setPeople, people }) {
 
   useEffect(() => {
     const getCategories = async () => {
-      const response = await axios.get("/yelpCategories/", {
+      const response = await axios.get("/v3/categories", {
         headers: {
           Authorization: `Bearer ${process.env.REACT_APP_YELP_API_KEY}`,
           withCredentials: true,
